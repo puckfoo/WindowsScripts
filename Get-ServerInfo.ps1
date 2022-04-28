b98eb671712d70d3835a57fa88d01d7a4ab8806b
Richard Holland <richard.k.holland@outlook.com>
2022-04-28 14:56:51 -0500

    Added error handling

--- Get-ServerInfo.ps1
+++ Get-ServerInfo.ps1
@@ -1,16 +1,43 @@
-function Get-ServerInfo {
+Function Get-ServerInfo {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
         [string]$ServerName,
         [Parameter(Mandatory = $true)]
-        [Validateset("domain01.com","domain02.com")]
+        [Validateset("domain01”,“domain02”,“domain02")]
         [string]$Domain
     )
-    
-    Get-ADObject -Filter {(ObjectClass -eq "Computer")} -Properties * -Server $Domain | Where-Object {$_.Name -eq $ServerName} | Format-Table Name, OperatingSystem, DNSHostName -AutoSize
+
+    try
+    {
+  
+     Get-ADObject -Filter {(ObjectClass -eq "Computer")} -Properties * -Server $Domain | Where {$_.Name -eq $ServerName } -Verbose | ft Name,OperatingSystem,DNSHostName -AutoSize
+
+    }
+    catch [System.Management.Automation.CommandNotFoundException]
+    {
+
+        if ($Error[0].FullyQualifiedErrorId) {
+            Write-Warning "Be sure the Admin tools are install, otherwise known as rsat"    #Checking for admin tools installation
+        }
+
+    }
+    catch [System.Security.Authentication.AuthenticationException]
+    {
+
+        if ($Error[0].FullyQualifiedErrorId) {
+            Write-Warning "Incorrect domain credentials"                                     #Checking for correct domain credentials
+        }
+
+    }
+    finally
+    {
+
+        $ErrorMessage = $_.Error.Message                                                     #Catch all
+        Write-Host $ErrorMessage -ForegroundColor Yellow
+
+    }
 
 }
 
 
-Get-ServerInfo -ServerName Server01 -Domain domain01.com
