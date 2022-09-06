451e82655c93e6bdcc9512a9d52b380dfdfbd308
Richard Holland <richard.k.holland@outlook.com>
2022-09-06 09:12:24 -0500

    dcpromo script

---
+++ dcpromo.ps1
@@ -0,0 +1,20 @@
+Start-Transcript -Path "D:\Misc\DCPromo.txt" -NoClobber
+Get-Date
+Get-WindowsFeature | Where-Object Installed | Format-Table Name
+Install-WindowsFeature –Name AD-Domain-Services -IncludeManagementTools
+Get-ADDomainController -Filter * | Format-Table Name, Site, IPv4Address -AutoSize
+Import-Module ADDSDeployment
+
+Install-ADDSDomainController `
+  -NoGlobalCatalog:$false `
+  -CreateDnsDelegation:$false `
+  -CriticalReplicationOnly:$false `
+  -DatabasePath "E:\NTDS" `
+  -DomainName "qa.umb.com" `   #change domain
+  -InstallDns:$false `
+  -LogPath "F:\NTDS" `
+  -NoRebootOnCompletion:$false `
+  -SysvolPath "E:\SYSVOL" `
+  -Confirm:$false `
+  -SafeModeAdministratorPassword `
+   (ConvertTo-SecureString 'u4I0HrfE' -AsPlainText -Force) 
