e5496adb610204d4dae6429c44f8db7d18113202
Richard Holland <richard.k.holland@outlook.com>
2023-03-31 14:16:58 -0500

    ps training

---
+++ Training01.ps1
@@ -0,0 +1,10 @@
+$variableOne = "1"
+$variableTwo = "3"
+$textVariable = $true
+
+($variableOne -eq $variableTwo) ? $true : $false
+($variableOne -eq $variableTwo) ?
+        ("$variableOne > $variableTwo ") : ("$variableOne < $variableTwo ")
+
+
+($textVariable) ? "Vaule is true" : "Value is false"
