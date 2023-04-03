00ebb3f732543683b624c328e37415af606ad630
Richard Holland <richard.k.holland@outlook.com>
2023-04-03 13:06:18 -0500

    update 01

--- Training02.ps1
+++ Training02.ps1
@@ -2,3 +2,17 @@ $array = 1..10
 $compare = 6
 
 if(($compare -in $array) -or ($compare -gt 8)) {Write-Host "Yes"} else {Write-Host "No"}
+
+*******************************************************************************************************
+
+$variable3 = 1
+$variable6 = 5
+
+if(($variable3 + $variable6) -gt 4) {Write-Host "Yes"}
+
+
+*******************************************************************************************************
+
+$variable1 = 1
+$variable2 = 3
+($variable1 -eq $variable2) ? $true : $false
