bc260506b5f326c223e9c264ae452808d940e504
Richard Holland <richard.k.holland@outlook.com>
2023-04-03 13:29:44 -0500

    bla

--- Training02.ps1
+++ Training02.ps1
@@ -16,3 +16,21 @@ if(($variable3 + $variable6) -gt 4) {Write-Host "Yes"}
 $variable1 = 1
 $variable2 = 3
 ($variable1 -eq $variable2) ? $true : $false
+
+
+*******************************************************************************************************
+<#
+Loops:
+
+for - for state and is interated through a specified number of times
+while - repeats a commands or group of commands where the given condition is true. 
+do-while - the body of the do while is executed first, then the condition is tested at the end
+do-until - the condition is evaluated first then the body of the code is run.
+foreach-object - 
+foreach 
+
+)
+
+
+
+#>
