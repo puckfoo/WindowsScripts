7fc6e72fa4e662dc5a278552f199bc266229a119
Richard Holland <richard.k.holland@outlook.com>
2023-04-14 09:35:27 -0500

    bla

--- Training04.ps1
+++ Training04.ps1
@@ -11,16 +11,19 @@ Common String Manipulation
 
 $variable1 = "The class instructor asked for a volunteer for a demon stration"
 
-
 $variable1 -replace "instructor","teacher"
 
 $replacevariable = $variable1 -replace "instructor","teacher"
 
 $replacevariable
 
+####################################################################################################
 
 $variable2 = "Jones Tom"
 
 $variable2 -replace "([a-z]+)\s([a-z]+)",'$2,$1'
 
-$variable2
+$variable2 -replace "([a-z]+)\s([a-z]+)",'$2 $1'
+
+$variable2 -replace '[^a-z]'
+
