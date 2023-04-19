b877766f87e1bdf1173680f9488e286b0ce95eb4
Richard Holland <richard.k.holland@outlook.com>
2023-04-19 09:48:28 -0500

    creating arrays

--- Training05.ps1
+++ Training05.ps1
@@ -3,6 +3,18 @@ Arrays
 
 What is an array - An array is a list or collection of values or objects.  Arrays only contain values not properties
 
-
+1. Empty arrays can be created by using "@()"
+2. Comma separated lists can also create Arrays
+3. The "Write-Output" command can create string arrays
 
 #>
+
+
+#Create Empty Array
+$variable = @()
+
+#Create Array with Text Values
+$variable = @('January','Feburary','March','April','May','June')
+
+#Create Array with Text Values not using @()
+$variable = 'January','Feburary','March','April','May','June')
