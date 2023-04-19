dcb0a5b2ac4826909f0d855a98f17bc70fd3626a
Richard Holland <richard.k.holland@outlook.com>
2023-04-19 11:08:14 -0500

    arrays

--- Training05.ps1
+++ Training05.ps1
@@ -11,10 +11,43 @@ What is an array - An array is a list or collection of values or objects.  Array
 
 
 #Create Empty Array
-$variable = @()
+$variable01 = @()
 
 #Create Array with Text Values
-$variable = @('January','Feburary','March','April','May','June')
+$variable02 = @('January','Feburary','March','April','May','June')
 
 #Create Array with Text Values not using @()
-$variable = 'January','Feburary','March','April','May','June')
+$variable03 = 'January','Feburary','March','April','May','June'
+
+#Create Array with Text Vaules not using "@90"
+$variable04 = Write-Output January Feburary March April May June 
+
+#Access Array Item using offset
+$variable03[3]
+$variable03[0,1,4]
+$variable03[2..5]
+
+$variable02 | ForEach-Object {"The month is: $PSItem"}
+
+#What is a $PSItem - It's a standdard PowerShell property or $variable that's created by PowerShell for a foreach object loop
+
+#Access Array Items using a foreach loop
+foreach ($item in $variable02) {"The month is: $item"}
+
+#Access Array Items using the foreach method
+$variable02.ForEach({"The month is: $PSItem"})
+
+
+#Access Array Items using for loop
+for ($item = 0; $item -lt $variable02.count; $item++) {
+    "The month is: {0}" -f $variable02[$item];
+    Write-Host "Current Position: $item"
+}
+
+
+
+
+
+
+
+
