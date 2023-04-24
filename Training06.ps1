dbe7cd7e5ae573c5040d817645dccd4c35843607
Richard Holland <richard.k.holland@outlook.com>
2023-04-24 14:09:44 -0500

    bla

--- Training06.ps1
+++ Training06.ps1
@@ -3,5 +3,45 @@
 Common Commands
 
 ConvertTo-Xml
+ConvertFrom-Json
+ConvertTo-Json
+
+Loading XML
+
+Simplest approach to use Select-XML command
+    Uses XPath queries to search for text in XML strings and documents
+
+Requires the PATH of the XML File and the XPATH within the XML document to search
 
 #>
+
+# Define Variables
+$path = 'C:\Users\Richard Holland\OneDrive\Documents\Data\Countries-with-Attributes.xml'
+$xpath = "/Countries/Country/Name"
+
+# Load Xml
+$xml = Select-Xml -Path $path -XPath $xpath
+
+
+
+# Define Variables
+$path = 'C:\Users\Richard Holland\OneDrive\Documents\Data\Countries-with-Attributes.xml'
+$xpath = "/Countries/Country/Name"
+
+# Load Xml 
+$xml = Select-Xml -Path $path -Xpath $xpath
+
+# Loop through Xml Property Vaules
+$xml | ForEach-Object {$_.Node.Name}
+
+
+# Create Xml variable and Load Data, then Retrieve Vaules
+[xml]$xml = Get-Content -Path $path
+$xml.Countries.Country.Name
+$xml.Countries.Country.Population
+
+
+
+
+
+
