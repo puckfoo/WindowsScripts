a628d3b52c150dd0b56bef2ae6c1e2d477095da3
Richard Holland <richard.k.holland@outlook.com>
2023-07-18 12:57:22 -0500

    bla

--- Get-Uptime.ps1
+++ Get-Uptime.ps1
@@ -1,12 +1,13 @@
 function Get-ComputerUptime {
     Param($ComputerName = $env:computername)
-    $os = Get-CimInstance -ClassName Win32_OperatingSystem -ComputerName $ComputerName
-    $up = New-TimeSpan -Start $os.LastBootUpTime -End (Get-Date)
-    [PSCustomObject]@{
-        ComputerName = $os.CSName
-        LastBoot     = $os.LastBootUpTime
-        Uptime       = $up
-    }
+        $os = Get-CimInstance -ClassName Win32_OperatingSystem -ComputerName $ComputerName
+        $up = New-TimeSpan -Start $os.LastBootUpTime -End (Get-Date)
+        
+        [PSCustomObject]@{
+            ComputerName = $os.CSName
+            LastBoot     = $os.LastBootUpTime
+            Uptime       = $up
+        }
 }
 
 Get-ComputerUptime
