f75c71138397ad7f6a5736e715e0ae8593422bb8
Richard Holland <richard.k.holland@outlook.com>
2023-08-02 10:25:46 -0500

    bla

--- Get-Weather.ps1
+++ Get-Weather.ps1
@@ -57,3 +57,28 @@ Describe "Get-OMCurrentWeather" {
         Get-OMCurrentWeather | Should -Be "YOUR_EXPECTED_VALUE"
     }
 }
+
+
+
+$r = Invoke-RestMethod 'https://api.open-meteo.com/v1/forecast?latitude=38.917&longitude=-94.0695&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum&temperature_unit=fahrenheit&precipitation_unit=inch&forecast_days=1&timezone=America%2FChicago'
+[PSCustomObject]@{
+    Location        = 'Overland Park, KS.'
+    Date            = $r.daily.time[0]
+    WeatherCode     = $r.daily.weathercode[0]
+    TemperatureLow  = $r.daily.temperature_2m_min[0]
+    TemperatureHigh = $r.daily.temperature_2m_max[0]
+    Precipitation   = $r.daily.precipitation_sum[0]
+    Sunrise         = $r.daily.sunrise[0] -as [datetime]
+    Sunset          = $r.daily.sunset[0] -as [datetime]
+}
+
+
+BeforeAll {
+    . $PSCommandPath.Replace('.Tests.ps1', '.ps1')
+}
+
+Describe "Get-OMCurrentWeather" {
+    It "Returns expected output" {
+        Get-OMCurrentWeather | Should -Be "YOUR_EXPECTED_VALUE"
+    }
+}
